bc51057d0b000a7263df529af3e29b40570e8ad5
Anthony Huang <anthill499@gmail.com>
2025-06-26 18:58:26 -0400

    nav bar changes

--- onboarding/src/NavBar.tsx
+++ onboarding/src/NavBar.tsx
@@ -4,20 +4,30 @@ import useFormStepper from "./hooks/useFormStepper";
 import "./index.css";
 
 function NavBar() {
-    const { user, logout } = useAuthConfig()
-    const { navigateToDefaultStep, currentStep } = useFormStepper()
+	const { user, logout } = useAuthConfig();
+	const { navigateToDefaultStep, currentStep } = useFormStepper();
 	let navigate = useNavigate();
-    let {pathname} = useLocation()
+	let { pathname } = useLocation();
 	return (
 		<nav>
 			<div>Zealthy Takehome</div>
-            {user != null && <div className="card-header">
-                <div>Welcome {user.firstName}</div>
-                <button onClick={logout}>Log Out</button>
-                {currentStep < 0 && <button onClick={navigateToDefaultStep}>Continue Onboarding</button>}
-                {user.isAdmin && <button onClick={() => navigate("/table")}>Table</button>}
-                {user.isAdmin && pathname !== '/admin' && <button onClick={() => navigate("/admin")}>Onboard Configuration</button>}
-            </div>}
+			{user != null && (
+				<div className='card-header'>
+					<div>Welcome {user.firstName}</div>
+					<button onClick={logout}>Log Out</button>
+					{currentStep < 0 && (
+						<button onClick={navigateToDefaultStep}>Continue Onboarding</button>
+					)}
+					{user.isAdmin && pathname !== "/table" && (
+						<button onClick={() => navigate("/table")}>Table</button>
+					)}
+					{user.isAdmin && pathname !== "/admin" && (
+						<button onClick={() => navigate("/admin")}>
+							Onboard Configuration
+						</button>
+					)}
+				</div>
+			)}
 		</nav>
 	);
 }
